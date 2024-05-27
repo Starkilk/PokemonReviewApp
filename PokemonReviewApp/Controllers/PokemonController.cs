93ae6e947d55cccdf6d23e6f8913f4a30212e5a1
Starlik <pavel.bolt@bk.ru>
2024-05-27 21:16:49 +0300

    PokemonDto и три Get  запроса в контроллере

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -19,12 +19,44 @@ namespace PokemonReviewApp.Controllers
         [ProducesResponseType(200, Type =  typeof(IEnumerable<Pokemon>))]
         public IActionResult GetPokemons()
         {
-            var pokempns = _pokemonRepository.GetPokemons();
+            var pokemons = _pokemonRepository.GetPokemons();
 
             if(!ModelState.IsValid) 
                 return BadRequest(ModelState);
 
-            return Ok(pokempns);
+            return Ok(pokemons);
+        }
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemons(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExist(pokeId))
+                return NotFound();
+
+            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if(!_pokemonRepository.PokemonExist(pokeId))
+                return NotFound();
+
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(rating);
         }
     }
 }
